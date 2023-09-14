616837241890c177aadc8d2d8a8b29fb392401f1
Pedro Burini <pulisses10@gmail.com>
2023-09-14 15:27:34 -0300

    creating the owner field using django's user model

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -17,8 +22,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
                                  blank=True, null=True)
+    owner = models.ForeignKey(User,
+                              on_delete=models.SET_NULL,
+                              blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
