f5ee5cf805d5df10e60ad29e41827fb1e63f4545
Pedro Burini <pulisses10@gmail.com>
2023-09-18 15:02:25 -0300

    injecting all contacts into the context of index.html template

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from contact.models import Contact
 
 # Create your views here.
 def index(request):
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
-        'contact',
+        'contact/index.html',
+        context
     )
