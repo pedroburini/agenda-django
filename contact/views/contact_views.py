2ce48b24520b3327a065bfe39b81a5f69524141b
Pedro Burini <pulisses10@gmail.com>
2023-09-18 14:36:28 -0300

    organizing view.py into a python package

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(
         request,
-        'contact/index.html',
+        'contact',
     )
--- project/settings.py
+++ project/settings.py
@@ -131,3 +131,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
+    
