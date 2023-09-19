f13b168c157da55f8ea5c6367d74db59bca63cde
Pedro Burini <pulisses10@gmail.com>
2023-09-19 14:25:45 -0300

    filtering values with q and or for the search field

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
 from contact.models import Contact
 
-# Create your views here.
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
@@ -16,18 +17,45 @@ def index(request):
         context
     )
 
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
     site_title = f'{single_contact.first_name} {single_contact.last_name} - '
-
     context = {
         'contact': single_contact,
         'site_title': site_title
     }
-
     return render(
         request,
         'contact/contact.html',
