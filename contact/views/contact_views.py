41c9cac3588e5766f1a88ea75f2ceab02bb3c94b
Pedro Burini <pulisses10@gmail.com>
2023-09-19 13:58:15 -0300

    creating partial files to use with include templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
         .order_by('-id')[10:20]
     context = {
         'contacts': contacts,
+        'site_title': 'Contacts - '
     }
     return render(
         request,
@@ -20,9 +21,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
