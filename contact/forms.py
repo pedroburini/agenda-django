a0509c35276d9f2fb7250ec395196b3dbd909fc5
Pedro Burini <pulisses10@gmail.com>
2023-09-20 15:34:30 -0300

    hiding non-owner update and delete links from contacts

--- contact/forms.py
+++ contact/forms.py
@@ -11,7 +11,8 @@ class ContactForm(forms.ModelForm):
             attrs={
                 'accept': 'image/*',
             }
-        )
+        ),
+        required=False
     )
 
     class Meta:
