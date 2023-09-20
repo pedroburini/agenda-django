54a57560ccc39f4e5e4fa6b1ed5c1ac0e240a496
Pedro Burini <pulisses10@gmail.com>
2023-09-20 14:31:22 -0300

    working with imagefield in template and forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -7,7 +7,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
 
         context = {
             'form': form,
@@ -43,7 +43,7 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
