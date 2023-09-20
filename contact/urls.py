48e52283d84a837652be8ab5a8353282cc72f99e
Pedro Burini <pulisses10@gmail.com>
2023-09-20 14:37:21 -0300

    using usercreationform to create new users in django

--- contact/urls.py
+++ contact/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     
+    # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
 ]
--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -2,4 +2,4 @@
 # type: ignore
 from .contact_views import *
 from .contact_forms import *
-
+from .user_forms import *
