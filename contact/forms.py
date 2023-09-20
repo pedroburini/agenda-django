5b0ac083cba13129bfeb4a8c019139d23248585c
Pedro Burini <pulisses10@gmail.com>
2023-09-20 15:16:02 -0300

    creating a form to update user data

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from . import models
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from . import models
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
@@ -34,7 +35,6 @@ class ContactForm(forms.ModelForm):
 
             self.add_error('first_name', msg)
             self.add_error('last_name', msg)
-        
         return super().clean()
     
     def clean_first_name(self):
@@ -48,7 +48,6 @@ class ContactForm(forms.ModelForm):
                     code='invalid'
                 )
             )
-
         return first_name
     
 class RegisterForm(UserCreationForm):
@@ -56,10 +55,12 @@ class RegisterForm(UserCreationForm):
         required=True,
         min_length=3,
     )
+
     last_name = forms.CharField(
         required=True,
         min_length=3,
     )
+
     email = forms.EmailField()
 
     class Meta:
@@ -75,8 +76,102 @@ class RegisterForm(UserCreationForm):
         if User.objects.filter(email=email).exists():
             self.add_error(
                 'email',
-                ValidationError('Já existe este e-mail', code='invalid')
+                ValidationError('this email already exists', code='invalid')
             )
 
         return email
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.',
+        error_messages={
+            'min_length': 'Please, add more than 2 letters.'
+        }
+    )
+    
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.'
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+
+    password2 = forms.CharField(
+        label="Password 2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text='Use the same password as before.',
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username',
+        )
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('passwords dont match')
+                )
+
+        return super().clean()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('this email already exists', code='invalid')
+                )
+
+        return email
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+
+        return password1
     
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
@@ -21,6 +21,32 @@ def register(request):
             'form': form
         }
     )
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    return redirect('contact:user_update')
 
 def login_view(request):
     form = AuthenticationForm(request)
