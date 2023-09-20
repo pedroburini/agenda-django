258f4481dacdda0376377e945460ea9feea4977a
Pedro Burini <pulisses10@gmail.com>
2023-09-20 15:01:25 -0300

    creating the login and logout system + authenticationform

--- contact/urls.py
+++ contact/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
 
     # user
     path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
 ]
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from contact.forms import RegisterForm
-
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     form = RegisterForm()
@@ -12,7 +12,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'registered user')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     return render(
         request,
@@ -21,3 +21,29 @@ def register(request):
             'form': form
         }
     )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'logged in successfully!')
+            return redirect('contact:index')
+        messages.error(request, 'invalid login')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
